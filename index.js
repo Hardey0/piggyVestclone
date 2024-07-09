350f92571cd066786942edcc9b04f9e8942cb141
onaolapoadesoye077@gmail.com <onaolapoadesoye077@gmail.com>
2024-07-09 02:31:54 -0700

    commit all

---
+++ index.js
@@ -0,0 +1,33 @@
+function openn(){
+
+  const openAde = document.getElementById('menu2');
+openAde.style.display="block";
+
+
+const openDe = document.getElementById('open-ade');
+openDe.style.display= 'none';
+
+const closeDe = document.getElementById('close-ade');
+closeDe.style.display= 'block';
+}
+
+function closee(){
+
+  const closeAde = document.getElementById('menu2');
+  closeAde.style.display= 'none';
+  
+  
+  const openDe = document.getElementById('open-ade');
+  openDe.style.display= 'block';
+  
+  const closeDe = document.getElementById('close-ade');
+  closeDe.style.display= 'none';
+  }
+function openn() {
+  document.getElementById('menu2').style.display = 'flex';
+}
+
+function closee() {
+  document.getElementById('menu2').style.display = 'none';
+}
+o
